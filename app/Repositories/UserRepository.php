de81e55fe84d3dbc2d8d8eb3ee925cf364fb1113
Alex Baratieri <aleex.baratieri@gmail.com>
2022-07-19 21:53:22 -0300

    [FIX] Correção no filtro de estado

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -21,7 +21,7 @@ class UserRepository extends Repository implements Crud
                 $this->entity = $this->applyFilter($filter, $value);
             }
         }
-        
+
         return $this->entity->get();
     }
 
@@ -38,7 +38,7 @@ class UserRepository extends Repository implements Crud
         if($type === 'state') {
             return $this->entity->whereHas('address.city', function($query) use ($value) {
                 return $query->whereHas('state', function($query) use($value){
-                    return $query->orWhere('uf', $value)->orWhere('state', $value);
+                    return $query->Where('uf', $value)->orWhere('state', $value);
                 });
             });
         }
